21c843a825f4936937544716df1e7a84b10cea58
Mesut OZTURK <mesutoztrk@msn.com>
2017-10-10 14:56:00 +0300

    Initial commit

---
+++ app/src/main/java/com/mstztrk/j10_1lifecycle/MainActivity.java
@@ -0,0 +1,43 @@
+package com.mstztrk.j10_1lifecycle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtMesaj;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtMesaj = (TextView) findViewById(R.id.txtMesaj);
+        txtMesaj.setText(R.string.karsilama_html);
+        txtMesaj.setTextSize(25);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "System Ready on Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "On Resume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Ben Terminatör T100 - Destroyed", Toast.LENGTH_SHORT).show();
+    }
+}
